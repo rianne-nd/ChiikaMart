a03c190ab33cc906d9e7b429dfb6953b7dcb9032
Your GitHub Name <edriannejohn.puno.cics@ust.edu.ph>
2026-04-23 01:17:18 +0800

    Week 9: Added password hashing

--- model/registrationModel.php
+++ model/registrationModel.php
@@ -8,28 +8,45 @@
             $this->conn = $db; // Assign the database connection to the private variable $conn for use within the class methods.
         }
 
-        public function createRegistration ($fName, $lName) {
+        public function createRegistration ($fName, $lName, $dept) {
+
             try {
-                // 1. Create the SQL query to insert a new registration record into the database. The query includes placeholders for the first name, last name, created at timestamp, and updated at timestamp. By using placeholders (e.g., :firstName), we can later bind the actual values to these parameters when executing the query, which helps prevent SQL injection attacks and allows for more secure database interactions.
+                // 1. Create the SQL query to insert a new registration record into the database. The query includes placeholders for the first name, last name, department, created at timestamp, and updated at timestamp. By using placeholders (e.g., :firstName), we can later bind the actual values to these parameters when executing the query, which helps prevent SQL injection attacks and allows for more secure database interactions.
                 $query = "INSERT INTO tbl_registrations 
-                (firstName, lastName, createdAt, updatedAt) 
+                (firstName, lastName, department, createdAt, updatedAt) 
                 -- To insert the values into the database, we first use parameters (:firstName and :lastName) in the SQL query. Then later on, we will bind the actual values of $firstName and $lastName to these parameters.
-                VALUES (:firstName, :lastName, :createdAt, :updatedAt)";
+                VALUES (:firstName, :lastName, :department, :createdAt, :updatedAt)";
 
                 // 2. Prepare the SQL query using the database connection. This alows us to execute the query with the bound parameters later on. The prepare method is used to create a prepared statement, which can be executed multiple times with different parameter values. This helps improve performance and security when interacting with the database.
                 $response = $this->conn->prepare($query);
 
-                // 3. Bind the actual values of $fName and $lName to the corresponding parameters in the prepared statement. This step involves associating the values of $fName and $lName with the placeholders (:firstName and :lastName) defined in the SQL query. Additionally, we can also bind the current timestamps for createdAt and updatedAt parameters to ensure that the registration record is accurately timestamped when it is inserted into the database.
-                $response->bindParam(':firstName', $fName);
-                $response->bindParam(':lastName', $lName);
+                $password_hash = password_hash($fName, PASSWORD_ARGON2ID);
+
+                $password_check = password_verify($fName, $password_hash);
+
+                // Change based on use, just a sample. Add to login function later on.
+                $selectQuery = "SELECT * FROM TBL WHERE EMAIL = EMAIL";
+                if($selectQuery) {
+                    $password_check = password_verify($inputted_pass, $selectQuery.password);
+                    if( $password_check ) {
+                    //bahala na kayo
+                    }
+                    //bahala
+                }
+
 
                 // Workaround since createdAt and updatedAt wont work.
                 $dateNow = date('Y-m-d H:i:s'); // Get the current date and time in the format 'YYYY-MM-DD HH:MM:SS'
-                
+
+                // 3. Bind the actual values of $fName and $lName to the corresponding parameters in the prepared statement. This step involves associating the values of $fName and $lName with the placeholders (:firstName and :lastName) defined in the SQL query. Additionally, we can also bind the current timestamps for createdAt and updatedAt parameters to ensure that the registration record is accurately timestamped when it is inserted into the database.
+                $response->bindParam(':firstName', $fName);
+                $response->bindParam(':lastName', $lName);
+                $response->bindParam(':department', $dept);
+
                 $response->bindParam(':createdAt', $dateNow);
                 $response->bindParam(':updatedAt', $dateNow);
 
-                // 4. Execute the prepared statement to insert the new registration record into the database. This step involves running the SQL query with the bound parameters, which will add a new entry to the tbl_registrations table with the provided first name, last name, and timestamps for createdAt and updatedAt. If the execution is successful, the new registration record will be stored in the database, allowing us to later retrieve or manage this data as needed.
+                // 4. Execute the prepared statement to insert the new registration record into the database. This step involves running the SQL query with the bound parameters, which will add a new entry to the tbl_registrations table with the provided first name, last name, department, and timestamps for createdAt and updatedAt. If the execution is successful, the new registration record will be stored in the database, allowing us to later retrieve or manage this data as needed.
                 $response->execute();
                 return $response;
             } catch (PDOException $ex) {
